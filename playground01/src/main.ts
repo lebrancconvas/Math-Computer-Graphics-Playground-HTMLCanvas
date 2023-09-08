0efb592f583ade14cdfdcdc4cd4c8cebf43dea2f
lebrancconvas <lebrancconvas@gmail.com>
2023-09-08 22:08:31 +0700

    ✨ feat: draw shape and calculate delta time and fps

--- playground01/src/main.ts
+++ playground01/src/main.ts
@@ -1,4 +1,5 @@
 import { canvas, ctx, config } from './canvas';
+import { Shape } from './components';
 
 // Prepare the canvas.
 canvas.width = config.width;
@@ -8,14 +9,27 @@ canvas.style.backgroundColor = config.backgroundColor;
 // Code.
 
 // Init.
+let timeStamp = 0;
+let lastTime = 0;
 
+const circle = new Shape(canvas.width / 2, 30, 30, 'Green');
 
 // Game Loop.
-function animate() {
+function animate(timeStamp: number) {
   window.requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  // Animation Code.
+
+  // Timer.
+  const deltaTime = timeStamp - lastTime;
+  lastTime = timeStamp;
+
+  console.log(`Delta Time: ${deltaTime} milliseconds`);
+  console.log(`FPS: ${1000 / deltaTime}`);
+
+  // Animation Logic.
+  circle.update();
+  circle.draw(ctx);
 
 };
 
-animate();
+animate(timeStamp);
